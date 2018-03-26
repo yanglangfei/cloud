1925630497e803cbe2bec3d791dd4b921fa74e39
Yanglangfei <yanglangfei@jucaipen.com>
2018-03-26 09:29:19 +0800

    task

--- task/src/main/java/com/yf/task/DrawCashTask.java
+++ task/src/main/java/com/yf/task/DrawCashTask.java
@@ -3,17 +3,18 @@ package com.yf.task;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 @Slf4j
 public class DrawCashTask {
 
-    @Scheduled(fixedRate = 60000)
-    public  void  run(){
-        log.info("task is runing:"+ LocalDate.now());
+    private SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+    @Scheduled(cron = "0 0/10 * * * ? ")
+    public  void  run(){
+        log.info("task is runing:"+ sdf.format(new Date()));
     }
 
 }
