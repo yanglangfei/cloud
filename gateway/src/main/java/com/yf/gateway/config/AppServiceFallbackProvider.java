b33e0a541eb033eb3e0d0f45effcb90bc552dc31
Yanglangfei <yanglangfei@jucaipen.com>
2018-03-29 14:24:26 +0800

    日志  分级配置

--- gateway/src/main/java/com/yf/gateway/config/AppServiceFallbackProvider.java
+++ gateway/src/main/java/com/yf/gateway/config/AppServiceFallbackProvider.java
@@ -7,9 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
-
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
@@ -27,17 +25,17 @@ public class AppServiceFallbackProvider implements ZuulFallbackProvider {
     public ClientHttpResponse fallbackResponse() {
         return new ClientHttpResponse() {
             @Override
-            public HttpStatus getStatusCode() throws IOException {
+            public HttpStatus getStatusCode() {
                 return HttpStatus.OK;
             }
 
             @Override
-            public int getRawStatusCode() throws IOException {
+            public int getRawStatusCode() {
                 return this.getStatusCode().value();
             }
 
             @Override
-            public String getStatusText() throws IOException {
+            public String getStatusText() {
                 return this.getStatusCode().getReasonPhrase();
             }
 
--- service/product-service/src/test/java/com/yf/ProductServerApplicationTests.java
+++ service/product-service/src/test/java/com/yf/ProductServerApplicationTests.java
@@ -1,16 +1,31 @@
 package com.yf;
 
+import com.yf.model.TbProduct;
+import com.yf.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ProductServiceApplication.class)
+@Slf4j
 public class ProductServerApplicationTests {
+
+	@Autowired
+	private ProductService productService;
 
 	@Test
 	public void contextLoads() {
+
+		List<TbProduct> products = productService.findAll();
+		log.info("products {}",products);
+
+
 	}
 
 }
