a86c604bb620a9daa0d5808c6cfce4be05828fcb
Yanglangfei <yanglangfei@jucaipen.com>
2018-03-30 13:54:59 +0800

    添加 缓存

--- service/product-service/src/main/java/com/yf/ProductServiceApplication.java
+++ service/product-service/src/main/java/com/yf/ProductServiceApplication.java
@@ -1,11 +1,13 @@
 package com.yf;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringCloudApplication
 @EnableFeignClients("com.yf.client")
+@EnableCaching
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- service/product-service/src/main/java/com/yf/mapper/TbProductMapper.java
+++ service/product-service/src/main/java/com/yf/mapper/TbProductMapper.java
@@ -2,11 +2,18 @@ package com.yf.mapper;
 
 import com.yf.lib.mapper.BaseMapper;
 import com.yf.model.TbProduct;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@CacheConfig(cacheNames = "AllProduct")
 public interface TbProductMapper extends BaseMapper<TbProduct> {
 
 
-
+    @Override
+    @Cacheable
+    List<TbProduct> findAll();
 }
