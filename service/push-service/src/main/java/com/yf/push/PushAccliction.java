05b3905942ca645b6adbbc834208f7448696153d
Yanglangfei <yanglangfei@jucaipen.com>
2018-04-04 15:31:22 +0800

    更新  service

---
+++ service/push-service/src/main/java/com/yf/push/PushAccliction.java
@@ -0,0 +1,13 @@
+package com.yf.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushAccliction {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushAccliction.class, args);
+    }
+
+}
