559a4e36f29298544d8e8dacddaa7e9fc4a165fa
Yanglangfei <yanglangfei@jucaipen.com>
2018-04-04 13:43:33 +0800

    1

--- web-yf/src/main/java/com/yf/web/WebApplication.java
+++ web-yf/src/main/java/com/yf/web/WebApplication.java
@@ -12,4 +12,30 @@ public class WebApplication {
 
     }
 
+
+    /**
+     * 启动应用
+     **/
+    public void start(){
+        // 第一步：启动应用服务……
+
+        // 第二步：注册JDK钩子
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("The hook running...");
+                //第三步：调用停机处理
+                stop();
+            }
+        }));
+    }
+
+    /**
+     * 停止应用
+     **/
+    public void stop(){
+        // 停止应用前停机处理(如：注销服务、标记不接受请求等)
+    }
+
+
 }
