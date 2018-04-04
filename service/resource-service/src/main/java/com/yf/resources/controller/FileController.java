0e1fd3e4f80b24bf6f5f17f500fb05783722be56
Yanglangfei <yanglangfei@jucaipen.com>
2018-04-04 17:15:57 +0800

    文件服务

--- service/resource-service/src/main/java/com/yf/resources/controller/FileController.java
+++ service/resource-service/src/main/java/com/yf/resources/controller/FileController.java
@@ -1,24 +1,33 @@
 package com.yf.resources.controller;
 
 import com.yf.resources.config.FastDFSClientWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
+@Slf4j
+@RequestMapping("/file")
 public class FileController {
 
     @Autowired
     private FastDFSClientWrapper fastDFSClientWrapper;
 
-    public void  uploadFile(MultipartFile multipartFile){
+    @GetMapping("/upload")
+    public String  uploadFile(MultipartFile multipartFile){
         try {
             String thumbImage = fastDFSClientWrapper.uploadImageAndCrtThumbImage(multipartFile);
+            log.info("上传成功: {} ",thumbImage);
+            return thumbImage;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return "上传失败";
     }
 
 }
