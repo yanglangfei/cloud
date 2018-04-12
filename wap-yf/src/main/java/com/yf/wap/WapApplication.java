ae8387acab98aa3ca52f0fdfd4df6cd1a9c7e113
Yanglangfei <yanglangfei@jucaipen.com>
2018-04-12 15:43:56 +0800

    wap

---
+++ wap-yf/src/main/java/com/yf/wap/WapApplication.java
@@ -0,0 +1,12 @@
+package com.yf.wap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WapApplication.class, args);
+	}
+}
